6b41a923c06248cc29e61c1e6d2a4ad49f6aeea9
Anxile <onebox.chen@outlook.com>
2025-03-12 21:11:19 -0400

    code clean

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('submit/', views.submit_form, name='submit_form'),
-    path('match_result/', views.match_result, name='match_result'),
 ]
